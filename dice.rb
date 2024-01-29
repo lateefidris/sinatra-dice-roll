e0052ba94194015f2e0044e3978c545c540038cd
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-01-29 08:53:07 +0000

    Added Silent Erb Tags

--- dice.rb
+++ dice.rb
@@ -28,15 +28,8 @@ end
 # DICE GAME
 
 # Home Page
-get("/dice") do
-  "<h1>Dice Roll</h1>
-  <ul>
-    <li><a href=/dice/2/6>Roll two 6-sided dice</a></li>
-    <li><a href=/dice/2/10>Roll two 10-sided dice</a></li>
-    <li><a href=/dice/1/20>Roll one 20-sided die</a></li>
-    <li><a href=/dice/5/4>Roll five 4-sided dice</a></li>
-    <li><a href=/dice/3/25>Roll three 25-sided dice</a></li>
-  </ul>"
+get("/") do
+  erb(:homepage)
 end
 
 # Two six-sided dice
@@ -46,10 +39,9 @@ get("/dice/2/6") do
 
   sum = first_die + second_die
   
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end  
 
 # Two ten-sided dice
@@ -59,20 +51,18 @@ get("/dice/2/10") do
 
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 # One twenty-sided die
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @die = rand(1..20)
 
-  outcome = "You rolled a #{first_die}"
+  @outcome = "You rolled a #{@die}"
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 # Five four-sided dice
@@ -85,10 +75,9 @@ get("/dice/5/4") do
 
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "First you rolled a #{first_die}, then you rolled a #{second_die}, then you rolled a #{third_die}, after that you rolled a #{fourth_die}, and finally you rolled a #{fifth_die} for a total of #{sum}."
+  @outcome = "First you rolled a #{first_die}, then you rolled a #{second_die}, then you rolled a #{third_die}, after that you rolled a #{fourth_die}, and finally you rolled a #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 end
 
 # Three twenty-five sided dice
@@ -99,8 +88,21 @@ get("/dice/3/25") do
 
   sum = first_die + second_die + third_die
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, and finally a #{third_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, a #{second_die}, and finally a #{third_die} for a total of #{sum}."
 
-  "<h1>3d25</h1>
-  <p>#{outcome}</p>"
+ erb(:three_twentyfive)
+end
+
+# One hundred six-sided dice
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+ 
 end
